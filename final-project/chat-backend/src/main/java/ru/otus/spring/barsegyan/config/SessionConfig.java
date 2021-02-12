7c990a908d19568b50b41d20c724a01661dc4e48
mihansweatpants <mihansweatpants@gmail.com>
2021-02-12 23:27:05 +0300

    [final-project--deploy] WIP

--- final-project/chat-backend/src/main/java/ru/otus/spring/barsegyan/config/SessionConfig.java
+++ final-project/chat-backend/src/main/java/ru/otus/spring/barsegyan/config/SessionConfig.java
@@ -1,10 +1,8 @@
 package ru.otus.spring.barsegyan.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HttpSessionIdResolver;
@@ -13,15 +11,8 @@ import org.springframework.session.web.http.HttpSessionIdResolver;
 @EnableRedisHttpSession
 public class SessionConfig {
     @Bean
-    public LettuceConnectionFactory connectionFactory(@Value("${spring.redis.host}") String redisHost,
-                                                      @Value("${spring.redis.port}") int redisPort,
-                                                      @Value("${spring.redis.password:null}") String redisPassword) {
-        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
-        redisConfiguration.setHostName(redisHost);
-        redisConfiguration.setPort(redisPort);
-        redisConfiguration.setPassword(redisPassword);
-
-        return new LettuceConnectionFactory(redisConfiguration);
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
     }
 
     @Bean
